3d01034ab10d23418d8c8af7dc3581f79ebdb1d1
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-16 04:59:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let newID = oldID +1000000000
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber =Math.random()
 let randomInteger = Math.floor(randomNumber)
+let randomUserID = randomInteger+1000000000
